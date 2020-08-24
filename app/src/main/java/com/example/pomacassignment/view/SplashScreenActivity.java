18371620064c54d5e545352c36e1884758d55cc8
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-08-24 22:38:35 +0200

    Handling if response is an error

--- app/src/main/java/com/example/pomacassignment/view/SplashScreenActivity.java
+++ app/src/main/java/com/example/pomacassignment/view/SplashScreenActivity.java
@@ -2,10 +2,14 @@ package com.example.pomacassignment.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.example.pomacassignment.R;
 import com.example.pomacassignment.model.Article;
 import com.example.pomacassignment.view_model.ArticlesViewModel;
@@ -16,16 +20,34 @@ import java.util.List;
 public class SplashScreenActivity extends AppCompatActivity {
 
     ArticlesViewModel articlesViewModel;
+    LottieAnimationView animationView;
+    TextView errorTv;
+    ImageView refreshIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        initView();
         initListeners();
 
         articlesViewModel.getArticlesList();
 
+    }
+
+    private void initView() {
+        animationView = findViewById(R.id.lottie);
+        errorTv = findViewById(R.id.tv_error_occur);
+        refreshIv = findViewById(R.id.iv_refresh);
+        refreshIv.setOnClickListener(view -> {refreshClicked();});
+    }
+
+    private void refreshClicked() {
+        articlesViewModel.getArticlesList();
+        animationView.setVisibility(View.VISIBLE);
+        errorTv.setVisibility(View.GONE);
+        refreshIv.setVisibility(View.GONE);
     }
 
     private void initListeners() {
@@ -35,8 +57,16 @@ public class SplashScreenActivity extends AppCompatActivity {
         articlesViewModel.returnArticles().observe(this, articles -> {
             if (articles != null)
                 goToHomeScreen(articles);
+            else
+                showErrorOccurred();
         });
 
+    }
+
+    private void showErrorOccurred() {
+        animationView.setVisibility(View.GONE);
+        errorTv.setVisibility(View.VISIBLE);
+        refreshIv.setVisibility(View.VISIBLE);
     }
 
     private void goToHomeScreen(List<Article> articles) {
