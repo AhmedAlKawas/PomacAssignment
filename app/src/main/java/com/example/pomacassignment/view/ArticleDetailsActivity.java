0141131e3a29c45c57b08fcf9b516650a9fe5ce6
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-08-24 21:54:27 +0200

    Designing and implementing the details activity

--- app/src/main/java/com/example/pomacassignment/view/ArticleDetailsActivity.java
+++ app/src/main/java/com/example/pomacassignment/view/ArticleDetailsActivity.java
@@ -1,16 +1,33 @@
 package com.example.pomacassignment.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.example.pomacassignment.R;
+import com.example.pomacassignment.databinding.ActivityArticleDetailsBinding;
+import com.example.pomacassignment.model.Article;
 
 public class ArticleDetailsActivity extends AppCompatActivity {
+
+    private ActivityArticleDetailsBinding articleDetailsBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_article_details);
+        articleDetailsBinding = DataBindingUtil.setContentView(this,
+                R.layout.activity_article_details);
+        getIntentExtras();
+        articleDetailsBinding.ivBack.setOnClickListener(view -> {
+            super.onBackPressed();
+        });
+    }
+
+    private void getIntentExtras() {
+        if (getIntent().getSerializableExtra(getString(R.string.articles)) != null){
+            articleDetailsBinding.setArticle((Article) getIntent()
+                    .getSerializableExtra(getString(R.string.articles)));
+        }
     }
 }
