ad01f6ecf09843de5c486b583df3c2e69394fbaf
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2020-08-24 17:36:51 +0200

    Checking that the recieved articles list is not null

--- app/src/main/java/com/example/pomacassignment/view/SplashScreenActivity.java
+++ app/src/main/java/com/example/pomacassignment/view/SplashScreenActivity.java
@@ -2,17 +2,16 @@ package com.example.pomacassignment.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.pomacassignment.R;
+import com.example.pomacassignment.model.Article;
 import com.example.pomacassignment.view_model.ArticlesViewModel;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -34,10 +33,15 @@ public class SplashScreenActivity extends AppCompatActivity {
         articlesViewModel = new ViewModelProvider(this).get(ArticlesViewModel.class);
 
         articlesViewModel.returnArticles().observe(this, articles -> {
-            Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
-            intent.putExtra(getString(R.string.articles), (Serializable) articles);
-            startActivity(intent);
+            if (articles != null)
+                goToHomeScreen(articles);
         });
 
+    }
+
+    private void goToHomeScreen(List<Article> articles) {
+        Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+        intent.putExtra(getString(R.string.articles), (Serializable) articles);
+        startActivity(intent);
     }
 }
